e06ba4796b9048147f88dbe0c507c96319c1776a
siltea2019 <siltea2019@gmail.com>
2025-02-16 23:27:23 +0900

    example: 3단원_this_01_예제추가

---
+++ 03_this/3-1-5_ex3-12.js
@@ -0,0 +1,15 @@
+// setTimeout
+setTimeout(function () {
+	console.log(this);
+}, 300);
+
+// forEach
+[1, 2, 3, 4, 5].forEach(function (x) {
+	console.log(this, x);
+});
+
+// DOM
+document.body.innerHTML += '<button id="a">클릭</button>';
+document.body.querySelector('#a').addEventListener('click', function (e) {
+	console.log(this, e);
+});
