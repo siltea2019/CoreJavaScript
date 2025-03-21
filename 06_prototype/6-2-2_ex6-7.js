a5dd203afff8cd3d02f1e439b024cc77b0882493
siltea2019 <siltea2019@gmail.com>
2025-03-21 20:49:03 +0900

    chore: 6단원_prototype_6-7예제 수정

--- 06_prototype/6-2-2_ex6-7.js
+++ 06_prototype/6-2-2_ex6-7.js
@@ -1,8 +1,8 @@
 var arr = [1, 2]
 
 arr.push(3)
-// arr.(__proto__).push
+// arr(.__proto__).push(3)
 
 arr.hasOwnProperty(2) // true
-// arr.(__proto__)(.proto__).hasOwnProperty
+// arr(.__proto__)(.__proto__).hasOwnProperty(2)
 
